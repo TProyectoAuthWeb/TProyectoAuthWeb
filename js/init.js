222fe2856b0d369f295d63069e5e25ade36b47fe
TProyectoAuthWeb <84989501+TProyectoAuthWeb@users.noreply.github.com>
2021-05-28 14:18:56 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCuGQqi4wcVg-vMxBpIgab-WRDS-d-7SC4",
+    authDomain: "fbpweb01auth.firebaseapp.com",
+    projectId: "fbpweb01auth",
+    storageBucket: "fbpweb01auth.appspot.com",
+    messagingSenderId: "449540059013",
+    appId: "1:449540059013:web:2b5aeb19d856cea56c5eec",
+    measurementId: "G-4C2YSYBQJ4"
+      });
